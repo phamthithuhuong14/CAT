bd4ae0ebf7956b76307215024f5c9279ddeb1b87
phamthithhuong14 <huongpttph34059@fpt.edu.vn>
2024-12-31 15:37:44 +0700

    fix

--- src/Views/HomeView.vue
+++ src/Views/HomeView.vue
@@ -21,35 +21,34 @@
       </div>
 
       <div class="loading-bar-container">
-        <div class="loading-bar" ></div>
+        <div class="loading-bar"></div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  export default {
-    data () {
-      return {
-        loadingProgress: 0
+export default {
+  data() {
+    return {
+      loadingProgress: 0,
+    };
+  },
+  methods: {
+    updateLoadingProgress() {
+      if (this.loadingProgress < 100) {
+        this.loadingProgress += 1;
+        setTimeout(this.updateLoadingProgress, 20);
+      } else {
+        this.$router.push("/daily");
       }
     },
-    methods: {
-      updateLoadingProgress () {
-        if(this.loadingProgress < 100) {
-          this.loadingProgress += 1
-          setTimeout(this.updateLoadingProgress, 20)
-        }else {
-          this.$router.push('/daily')
-        }
-      }
-    },
-    mounted () {
-      this.updateLoadingProgress()
-    }
-  }
+  },
+  mounted() {
+    this.updateLoadingProgress();
+  },
+};
 </script>
-
 
 <style scoped>
 .container {
@@ -64,68 +63,67 @@
 
 main {
   background: rgba(255, 249, 233, 1);
-  
 }
 
-.cat-image{
+.cat-image {
   width: 400px;
   height: 266px;
   margin-top: 30%;
 }
 
-.cat-hi img{
+.cat-hi img {
   margin-right: 50px;
   width: 60px;
   height: 60px;
 }
 
 .loading-text {
-    color: rgba(99, 19, 19, 1);
-    font-size: 28px;
-    font-family: Irish Grover;
-    font-weight: 400;
-    margin-top: 123px;
-    text-align: center;
-  }
+  color: rgba(99, 19, 19, 1);
+  font-size: 28px;
+  font-family: Irish Grover;
+  font-weight: 400;
+  margin-top: 123px;
+  text-align: center;
+}
 
 .loading-bar {
-    height: 100%;
-    border-radius: 10px;
-    transition: width 0.2s ease;
-    border: 1.5px solid rgba(99, 19, 19, 1);
-    animation: progress 2s linear infinite; /* Tạo animation */
-    background: linear-gradient(180deg, #bf5828 0%, #592913 100%);
-  }
+  height: 100%;
+  border-radius: 10px;
+  transition: width 0.2s ease;
+  border: 1.5px solid rgba(99, 19, 19, 1);
+  animation: progress 2s linear infinite; /* Tạo animation */
+  background: linear-gradient(180deg, #bf5828 0%, #592913 100%);
+}
 
-  .loading-bar-container {
-    width: 80%;
-    height: 22px;
-    background-color: #f0ddd2;
-    border-radius: 10px;
-    overflow: hidden;
-    margin-bottom: 10px;
-    border: 1.5px solid rgba(99, 19, 19, 1);
-    background: rgba(255, 225, 186, 1);
-  }
+.loading-bar-container {
+  width: 80%;
+  height: 22px;
+  background-color: #f0ddd2;
+  border-radius: 10px;
+  overflow: hidden;
+  margin-bottom: 10px;
+  border: 1.5px solid rgba(99, 19, 19, 1);
+  background: rgba(255, 225, 186, 1);
+}
 
-  .loading-bar {
-    height: 100%;
-    border-radius: 10px;
-    transition: width 0.2s ease;
-    border: 1.5px solid rgba(99, 19, 19, 1);
-    animation: progress 2s linear infinite; /* Tạo animation */
-    background: linear-gradient(180deg, #bf5828 0%, #592913 100%);
-    transition: width 0.3s ease;
-  }
+.loading-bar {
+  height: 100%;
+  border-radius: 10px;
+  transition: width 0.2s ease;
+  border: 1.5px solid rgba(99, 19, 19, 1);
+  animation: progress 2s linear infinite; /* Tạo animation */
+  background: linear-gradient(180deg, #bf5828 0%, #592913 100%);
+  transition: width 0.3s ease;
+}
 
-  @keyframes progress {
-    0% {
-      width: 0%;
-    }
-    100% {
-      width: 100%;
-    }
+@keyframes progress {
+  0% {
+    width: 0%;
   }
+  100% {
+    width: 100%;
+  }
+}
 
 @media screen and (max-width: 700px) {
   .container {
@@ -141,7 +139,6 @@ main {
     display: flex;
     flex-direction: column;
     align-items: center;
-    width: 100%;
     /* height: 193vh; */
   }
 
@@ -168,11 +165,10 @@ main {
 
   .loader-container {
     display: flex;
-
     align-items: center;
     justify-content: center;
     width: 100%;
-    height: 820px;
+    margin-top: 40%;
   }
 
   .cat-container {
@@ -181,8 +177,7 @@ main {
 
   .cat-hi img {
     width: 100%;
-    margin-bottom: 79px;
-    margin-right: 101px;
+   
   }
 
   .cat-image {
@@ -217,8 +212,7 @@ main {
     font-size: 32px;
     font-family: Irish Grover;
     font-weight: 400;
-    margin-top: 346px;
-    
+    margin-top: 500px;
   }
 
   @keyframes progress {
